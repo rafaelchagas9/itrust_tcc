b1b61affec43cfdd057245de60e3008e232b329b
BlackGamesHD <33136664+BlackGamesHD@users.noreply.github.com>
2019-11-25 17:54:58 -0300

    Fragmentos adicionados

--- app/src/main/java/com/iTrust/tcc/ui/fragmentos/ConselhosFragment.kt
+++ app/src/main/java/com/iTrust/tcc/ui/fragmentos/ConselhosFragment.kt
@@ -8,19 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.iTrust.tcc.R
-import kotlinx.android.synthetic.main.fragment_fragment_one.*
+import kotlinx.android.synthetic.main.fragment_conselhos.view.*
+import kotlinx.android.synthetic.main.fragment_recomendado.*
 
 class ConselhosFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_one, container, false)
+        return inflater.inflate(R.layout.fragment_conselhos, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fragmento_um_nome.text="Fragmento dois"
+        view.f1.text = "Não beber coca cola"
+        view.f2.text = "Evitar baforar lança depois de usar crack"
     }
 
 
 }
+
+
--- app/src/main/java/com/iTrust/tcc/ui/fragmentos/RecomendadoFragment.kt
+++ app/src/main/java/com/iTrust/tcc/ui/fragmentos/RecomendadoFragment.kt
@@ -8,14 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.iTrust.tcc.R
-import kotlinx.android.synthetic.main.fragment_fragment_one.*
+import kotlinx.android.synthetic.main.fragment_recomendado.*
 
 class RecomendadoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_one, container, false)
+        return inflater.inflate(R.layout.fragment_recomendado, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
